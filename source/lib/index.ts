1a342c91bdf864f432ab5b62cf9b04e649786fe4
Davi Barreto <davivbarreto@gmail.com>
2020-08-18 09:24:47 +0300

    Add sampling hashing implementation. TODO: Sync version and fix tests

--- source/lib/index.ts
+++ source/lib/index.ts
@@ -9,6 +9,11 @@ import {
     readdir,
     readFile,
     lstat,
+    openSync,
+    readSync,
+    closeSync,
+    createReadStream,
+    promises,
 } from "fs";
 import { promisify } from "util";
 
@@ -639,9 +644,9 @@ async function _scanAsync(
                 dirTree.size = options.size ? parseSize(size) : undefined;
             }
             if (options.hash) {
-                let data: Buffer;
+                let data: string;
                 try {
-                    data = await readFileAsync(path);
+                    data = await hashFile(path);
                 } catch (exception) {
                     /* istanbul ignore next */
                     if (options.skipErrors) {
@@ -1168,3 +1173,54 @@ export async function parseTreeAsync(
         : "";
     return result;
 }
+
+const hashFile = async (path, threshold = 128 * 1024) => {
+    // get file size
+    const stat = promises.stat(path);
+    const size = (await stat).size;
+
+    // if smaller than threshold, hash the entire file
+    if (size < threshold) {
+        return hashPortion(path);
+    }
+
+    // else sample start, middle and file end
+    const chunkSize = Math.floor(threshold / 3);
+    const startHash = await hashPortion(path, { start: 0, end: chunkSize });
+
+    const midStart = Math.floor(size / 2);
+    const middleHash = await hashPortion(path, {
+        start: midStart,
+        end: midStart + chunkSize,
+    });
+
+    const endHash = await hashPortion(path, {
+        start: size - chunkSize,
+        end: undefined,
+    });
+
+    // return hash of all samplings
+    const output = createHash("md5");
+    output.update(startHash + middleHash + endHash);
+    return output.digest("hex");
+};
+
+const hashPortion = async (
+    path,
+    { start, end } = { start: 0, end: Infinity }
+): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const output = createHash("md5");
+        const input = createReadStream(path, { start, end });
+
+        input.on("error", (err) => {
+            reject(err);
+        });
+
+        output.once("readable", () => {
+            resolve(output.read().toString("hex"));
+        });
+
+        input.pipe(output);
+    });
+};
